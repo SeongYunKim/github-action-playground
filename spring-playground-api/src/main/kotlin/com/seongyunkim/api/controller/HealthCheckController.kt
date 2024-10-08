7e4c7ef821a3ff50912ff814cbcd5fcaecc59ae4
SeongYunKim <cauboy17@naver.com>
2024-10-08 19:18:09 +0900

    set spring.profiles.active based on env variable

--- spring-playground-api/src/main/kotlin/com/seongyunkim/api/controller/HealthCheckController.kt
+++ spring-playground-api/src/main/kotlin/com/seongyunkim/api/controller/HealthCheckController.kt
@@ -1,5 +1,6 @@
 package com.seongyunkim.api.controller
 
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -7,9 +8,11 @@ import java.time.ZonedDateTime
 
 @RequestMapping("/api/v1/healthcheck")
 @RestController
-class HealthCheckController() {
+class HealthCheckController(
+    private val environment: Environment,
+) {
     @GetMapping
     fun healthCheck(): String {
-        return ZonedDateTime.now().toString()
+        return "${environment.activeProfiles.first()}_${ZonedDateTime.now()}"
     }
 }
